519c078be6760e7a63f608b8b6cc7fa3a6623d73
Stalloyde <stalloyde93@gmail.com>
2023-01-09 13:10:28 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import _ from 'lodash';
+import './style.css';
+
 export function createDropDownMenu() {
-  console.log('asdasdsa');
+  const dashboard = document.getElementById('dashboard');
+  const dropDownMenu = document.querySelector('.dropdown-menu');
+  const dropDownMenuItems = dropDownMenu.querySelectorAll('.menu-item');
+
+  dashboard.addEventListener('click', () => {
+    dropDownMenu.classList.toggle('show-menu');
+
+    setTimeout(() => {
+      dropDownMenu.classList.toggle('trigger2');
+    }, 100);
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target.id !== dashboard.id) {
+      dropDownMenu.classList.remove('show-menu');
+      dropDownMenu.classList.remove('trigger2');
+    }
+  });
+
+  dropDownMenuItems.forEach((item) => {
+    const dropDownSubMenu = item.querySelector('.dropdown-submenu');
+    item.addEventListener('mouseover', () => {
+      dropDownSubMenu.classList.add('show-submenu');
+    });
+
+    item.addEventListener('mouseout', () => {
+      dropDownSubMenu.classList.remove('show-submenu');
+    });
+  });
 }
